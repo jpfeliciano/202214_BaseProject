ff8d95025154cc3879c2693983de9bcbbf85d849
Juan Feliciano <jp.feliciano10@uniandes.edu.co>
2022-09-18 21:23:00 -0500

    Se corrige error por dependencia no incluída en módulo.

--- src/ciudad-supermercado/ciudad-supermercado.module.ts
+++ src/ciudad-supermercado/ciudad-supermercado.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CiudadEntity } from 'src/ciudad/ciudad.entity';
 import { CiudadSupermercadoService } from './ciudad-supermercado.service';
 import { CiudadSupermercadoController } from './ciudad-supermercado.controller';
+import { SupermercadoEntity } from 'src/supermercado/supermercado.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CiudadEntity])],
+  imports: [TypeOrmModule.forFeature([CiudadEntity, SupermercadoEntity])],
   providers: [CiudadSupermercadoService],
   controllers: [CiudadSupermercadoController],
 })
